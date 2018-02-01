3ba7b342f85d3f34459fde69754b415b84e9e192
Sam Catherman <scatherman@VALM-GREGRTEMP2.hq.fool.net>
2018-02-01 17:09:32 -0500

    figure out form

--- src/App.js
+++ src/App.js
@@ -6,17 +6,44 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      username: ''
+      username: '',
+      img: '',
+      type: ''
     }
     this.handleClick = this.handleClick.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleClick(){}
+  handleClick(){
+   console.log('clicky');
+   axios.get(`https://api.github.com/users/samcatherman`)
+   .then(res => this.setState(
+     {
+       username: res.data.name,
+       img: res.data.avatar_url,
+       type: res.data.type
+     }
+   ))
+  }
+
+  handleSubmit(event){
+    console.log("A name was searched: " + this.state.username);
+    event.preventDefault()
+  }
 
   render() {
     return (
-      <div className="App">
-      <h1>Hello There</h1>
+      <div className="button_container">
+  <form onSubmit={this.handleSubmit}>
+    <label>
+      Enter a Github Username:
+    </label>
+    <input type="text" name="username"></input>
+  </form>
+        <button className='button' onClick={this.handleClick}>Find User</button>
+        <h3>{this.state.type}</h3>
+        <p>{this.state.username}</p>
+        <img src={this.state.img}/>
       </div>
     );
   }
